9e7f0044a65df3cef722e226b3b1a4553aa17685
James-Molzer <70172489+James-Molzer@users.noreply.github.com>
2021-03-29 23:18:35 -0400

    Add files via upload

---
+++ csharp-capstone-module-1-team-0/CapstoneTests/VendingMachineTest.cs
@@ -0,0 +1,30 @@
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneTests
+{
+    [TestClass]
+
+    public class VendingMachineTest
+    {
+
+        [TestMethod]
+
+        public void ItemExists_Test()
+        {
+            //            //Arrange
+            //            string itemCode = "A1";
+
+            //            VendingMachine vendingMachine = new VendingMachine();
+
+            //            //Act
+            //            bool itemExists = vendingMachine.ItemExists("itemCode");
+
+            //            //Assert
+            //            Assert.IsTrue(true, itemExists(itemCode);
+        }
+    }
+}
