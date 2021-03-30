7d0c83f421e07e338c044cda7e7ef481f87517cd
James-Molzer <70172489+James-Molzer@users.noreply.github.com>
2021-03-29 23:32:46 -0400

    Add files via upload

---
+++ csharp-capstone-module-3-team-5/TenmoServer/DAO/IAccountDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public interface IAccountDAO
+    {
+        Account GetAccounts(int id);
+        bool UpdatedBalance(int userId, decimal amount);
+    }
+}
---
+++ csharp-capstone-module-3-team-5/TenmoServer/DAO/ITransferDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public interface ITransferDAO
+    {
+        List<Transfer> GetTransactionsByID(int id);
+        Transfer GetTransactions(Transfer transfers);
+
+
+    }
+}
